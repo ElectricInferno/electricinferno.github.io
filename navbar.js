25ad44366127956ed6bf0db031841c49f5728e38
unknown <1004986@PLV-111402L.plv.esu3.net>
2014-11-25 08:50:47 -0600

    Added tooltips for Lessons 4-6, rewrote other tooltips.

--- navbar.js
+++ navbar.js
@@ -1,17 +1,5 @@
-function mouseOutHome() {
-	document.getElementById("home").firstChild.innerHTML="Home";
-	document.getElementById("home").firstChild.style.fontSize = "17px";
-	document.getElementById("home").firstChild.style.maxWidth = "115px";
-}
-
-function mouseOverHome() {
-	document.getElementById("home").firstChild.innerHTML = "You're already here, silly!";
-	document.getElementById("home").firstChild.style.maxWidth = "81px";
-	document.getElementById("home").firstChild.style.fontSize = "10px";
-}
-
 function mouseOverLesson1() {
-document.getElementById("lesson1").firstChild.innerHTML = "Learn how to get started with your webpage.";
+document.getElementById("lesson1").firstChild.innerHTML = "Getting Started";
 document.getElementById("lesson1").firstChild.style.maxWidth = "113px";
 document.getElementById("lesson1").firstChild.style.fontSize = "10px";
 }
@@ -23,7 +11,7 @@ document.getElementById("lesson1").firstChild.style.maxWidth = "115px";
 }
 
 function mouseOverLesson2() {
-document.getElementById("lesson2").firstChild.innerHTML = "Add some basic text to your webpage.";
+document.getElementById("lesson2").firstChild.innerHTML = "Add some basic text";
 document.getElementById("lesson2").firstChild.style.maxWidth = "113px";
 document.getElementById("lesson2").firstChild.style.fontSize = "10px";
 }
@@ -35,7 +23,7 @@ document.getElementById("lesson2").firstChild.style.maxWidth = "115px";
 }
 
 function mouseOverLesson3() {
-document.getElementById("lesson3").firstChild.innerHTML = "Add a navigation bar to your webpage.";
+document.getElementById("lesson3").firstChild.innerHTML = "Add some images";
 document.getElementById("lesson3").firstChild.style.maxWidth = "113px";
 document.getElementById("lesson3").firstChild.style.fontSize = "10px";
 }
@@ -44,4 +32,40 @@ function mouseOutLesson3() {
 document.getElementById("lesson3").firstChild.innerHTML="Lesson 3";
 document.getElementById("lesson3").firstChild.style.fontSize = "17px";
 document.getElementById("lesson3").firstChild.style.maxWidth = "115px";
+}
+
+function mouseOverLesson4() {
+document.getElementById("lesson4").firstChild.innerHTML = "Links and link disguises";
+document.getElementById("lesson4").firstChild.style.maxWidth = "113px";
+document.getElementById("lesson4").firstChild.style.fontSize = "10px";
+}
+
+function mouseOutLesson4() {
+document.getElementById("lesson4").firstChild.innerHTML="Lesson 4";
+document.getElementById("lesson4").firstChild.style.fontSize = "17px";
+document.getElementById("lesson4").firstChild.style.maxWidth = "115px";
+}
+
+function mouseOverLesson5() {
+document.getElementById("lesson5").firstChild.innerHTML = "Types of navigation";
+document.getElementById("lesson5").firstChild.style.maxWidth = "113px";
+document.getElementById("lesson5").firstChild.style.fontSize = "10px";
+}
+
+function mouseOutLesson5() {
+document.getElementById("lesson5").firstChild.innerHTML="Lesson 5";
+document.getElementById("lesson5").firstChild.style.fontSize = "17px";
+document.getElementById("lesson5").firstChild.style.maxWidth = "115px";
+}
+
+function mouseOverLesson6() {
+document.getElementById("lesson6").firstChild.innerHTML = "Lesson to be determined";
+document.getElementById("lesson6").firstChild.style.maxWidth = "113px";
+document.getElementById("lesson6").firstChild.style.fontSize = "10px";
+}
+
+function mouseOutLesson6() {
+document.getElementById("lesson6").firstChild.innerHTML="Lesson 6";
+document.getElementById("lesson6").firstChild.style.fontSize = "17px";
+document.getElementById("lesson6").firstChild.style.maxWidth = "115px";
 }
