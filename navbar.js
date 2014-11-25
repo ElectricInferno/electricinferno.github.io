6a47703c657a74712d5ab266932a23f4b24b230c
ElectricInferno <carter0906@gmail.com>
2014-11-24 20:35:57 -0600

    Initial Commit

---
+++ navbar.js
@@ -0,0 +1,47 @@
+function mouseOutHome() {
+	document.getElementById("home").firstChild.innerHTML="Home";
+	document.getElementById("home").firstChild.style.fontSize = "17px";
+	document.getElementById("home").firstChild.style.maxWidth = "115px";
+}
+
+function mouseOverHome() {
+	document.getElementById("home").firstChild.innerHTML = "You're already here, silly!";
+	document.getElementById("home").firstChild.style.maxWidth = "81px";
+	document.getElementById("home").firstChild.style.fontSize = "10px";
+}
+
+function mouseOverLesson1() {
+document.getElementById("lesson1").firstChild.innerHTML = "Learn how to get started with your webpage.";
+document.getElementById("lesson1").firstChild.style.maxWidth = "113px";
+document.getElementById("lesson1").firstChild.style.fontSize = "10px";
+}
+
+function mouseOutLesson1() {
+document.getElementById("lesson1").firstChild.innerHTML="Lesson 1";
+document.getElementById("lesson1").firstChild.style.fontSize = "17px";
+document.getElementById("lesson1").firstChild.style.maxWidth = "115px";
+}
+
+function mouseOverLesson2() {
+document.getElementById("lesson2").firstChild.innerHTML = "Add some basic text to your webpage.";
+document.getElementById("lesson2").firstChild.style.maxWidth = "113px";
+document.getElementById("lesson2").firstChild.style.fontSize = "10px";
+}
+
+function mouseOutLesson2() {
+document.getElementById("lesson2").firstChild.innerHTML="Lesson 2";
+document.getElementById("lesson2").firstChild.style.fontSize = "17px";
+document.getElementById("lesson2").firstChild.style.maxWidth = "115px";
+}
+
+function mouseOverLesson3() {
+document.getElementById("lesson3").firstChild.innerHTML = "Add a navigation bar to your webpage.";
+document.getElementById("lesson3").firstChild.style.maxWidth = "113px";
+document.getElementById("lesson3").firstChild.style.fontSize = "10px";
+}
+
+function mouseOutLesson3() {
+document.getElementById("lesson3").firstChild.innerHTML="Lesson 3";
+document.getElementById("lesson3").firstChild.style.fontSize = "17px";
+document.getElementById("lesson3").firstChild.style.maxWidth = "115px";
+}
